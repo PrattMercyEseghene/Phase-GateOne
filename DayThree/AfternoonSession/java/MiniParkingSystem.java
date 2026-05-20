6d616bb585149cb890eb92e3f035330fef6f167f
PrattMercy <prattmercy3@gmail.com>
2026-05-20 16:02:46 +0100

    first commit

---
+++ DayThree/AfternoonSession/java/MiniParkingSystem.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class MiniParkingSystem{
+public static void main(String[]args){
+Scanner inputCollector = new Scanner(System.in);
+int[]parkingSlot = [20];
+
+
+
+
+}
+
+
+public static int showParkingSlot(int[]parkingSlot){
+System.out.println("parking status");
+
+for(int count=0; count<parkingSlot.length;count++){
+
+if(parkingSlot ==0){
+System.out.println("slot"+ count+"is empty");
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DayThree/AfternoonSession/java/MiniParkingSystemTest.java
@@ -0,0 +1,19 @@
+import org.junnit.jupiter.api.Test;
+import static org.junnit.jupiter.api.Assertion.*;
+
+public class MiniParkingSystemTest{
+@Test
+    public void testThatParkingSlotIsEmpty(){
+    
+    int[] parkingSlot=[0];
+    
+    int display park=0;
+    
+    int actualSlot = MiniParkingSystem.showParkingSlot
+    
+    
+    
+    
+    
+    }
+}
